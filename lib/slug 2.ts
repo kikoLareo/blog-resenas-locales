e723eeb71c313b3306cf80223465fc74b0c9974e
Kiko <kiko.lareo@gmail.com>
2025-09-09 22:11:07 +0200

    feat: add manifest file for PWA and seed script for Sanity content

---
+++ lib/slug 2.ts
@@ -0,0 +1,11 @@
+export function slugify(input: string): string {
+  return input
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '') // quitar acentos
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)+/g, '')
+    .replace(/-{2,}/g, '-');
+}
+
+
